bd17f0e45c4aa3b823ec3737559d6f01a6b195df
Duan <rlawndks30@gmail.com>
2024-01-02 00:52:31 +0900

    feat : kakao sso init

--- parkinson_motor_diary/lib/main.dart
+++ parkinson_motor_diary/lib/main.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:parkinson_motor_diary/sample_screen.dart';
 import 'calendar_screen.dart'; // 화면 1에 해당하는 위젯
 import 'screen2.dart'; // 화면 2에 해당하는 위젯
 import 'screen3.dart'; // 화면 3에 해당하는 위젯
+import 'config.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+
+  // Init Kakao SDK 
+    KakaoSdk.init(
+        nativeAppKey: KakaoConfig.nativeAppKey,
+        javaScriptAppKey: KakaoConfig.javaScriptAppKey
+    );
+
+  // Run App
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: CalendarScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SampleScreen(),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return const MaterialApp(
+  //     title: 'Flutter Demo',
+  //     home: CalendarScreen(),
+  //   );
+  // }
 }
